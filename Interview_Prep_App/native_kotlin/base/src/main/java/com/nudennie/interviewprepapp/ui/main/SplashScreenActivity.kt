3f426da69fa885b683fc255e9b53ebd960fb48e3
AndroidLion48 <>
2022-10-21 00:02:39 -0700

    Added a splash screen

---
+++ Interview_Prep_App/native_kotlin/base/src/main/java/com/nudennie/interviewprepapp/ui/main/SplashScreenActivity.kt
@@ -0,0 +1,30 @@
+package com.nudennie.interviewprepapp.ui.main
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.nudennie.interviewprepapp.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    // This holds the time in milisec for the splash screen display
+    val screenTimer = 3000L
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed(Runnable { // This method executes once timer has ended
+
+
+
+            // Starting app main activity
+            val i = Intent(this@SplashScreenActivity, MainActivity::class.java)
+            startActivity(i)
+
+            // Closing this activity
+            finish()
+
+        }, screenTimer)
+    }
+}
