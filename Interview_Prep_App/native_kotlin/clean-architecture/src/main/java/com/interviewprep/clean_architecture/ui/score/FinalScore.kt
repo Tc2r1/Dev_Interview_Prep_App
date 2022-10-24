23d2948f51b292eecb2e67ea778c7bd31229caea
JanFidor <jan.p.fidor@gmail.com>
2022-10-24 15:04:29 +0200

    add final score fragment

---
+++ Interview_Prep_App/native_kotlin/clean-architecture/src/main/java/com/interviewprep/clean_architecture/ui/score/FinalScore.kt
@@ -0,0 +1,12 @@
+package com.interviewprep.clean_architecture.ui.score
+
+import android.os.Parcelable
+
+@kotlinx.parcelize.Parcelize
+data class FinalScore(
+    val questionsCount: Int,
+    val correctAnswersCount: Int,
+) : Parcelable {
+    val percentageScore: Int
+        get() = (correctAnswersCount / questionsCount) * 100
+}
