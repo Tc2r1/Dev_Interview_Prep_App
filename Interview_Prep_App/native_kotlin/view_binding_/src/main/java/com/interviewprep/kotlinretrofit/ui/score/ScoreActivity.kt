9d7195a79735e802ac6627669135c72fd21cd21e
prasad-more-12 <prsadmore012@gmail.com>
2022-10-25 16:26:57 +0530

    copied the application module folder and renamed it

---
+++ Interview_Prep_App/native_kotlin/view_binding_/src/main/java/com/interviewprep/kotlinretrofit/ui/score/ScoreActivity.kt
@@ -0,0 +1,89 @@
+package com.interviewprep.kotlinretrofit.ui.score
+
+import android.content.Intent
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.interviewprep.kotlinretrofit.R
+import com.interviewprep.kotlinretrofit.ui.main.MainActivity
+
+/**
+ * Created by Tc2r on 5/31/2017.
+ *
+ *
+ * Description: Displays the score after a quiz is taken.
+ */
+class ScoreActivity : AppCompatActivity() {
+    // UI Variables
+    private var totalQuestionsTV: TextView? = null
+    private var totalCorrectTV: TextView? = null
+    private var finalGradeTV: TextView? = null
+    private var commentTV: TextView? = null
+
+    // Extras Variables
+    private var numCorrect = 0
+    private var quizSize = 0
+    private var scorePer = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Get Extras from intent
+        val extras = intent.extras
+
+        // Check if null
+        if (extras != null) {
+            // Assign Extras
+            scorePer = extras.getInt("scorePercentage", 0)
+            quizSize = extras.getInt("quizSize", 0)
+            numCorrect = extras.getInt("numCorrect", 0)
+        }
+        setContentView(R.layout.activity_score)
+
+        // Assign/initiate variables
+        totalQuestionsTV = findViewById(R.id.numofquestions_tv)
+        commentTV = findViewById(R.id.comment_tv)
+        totalCorrectTV = findViewById(R.id.numcorrect_tv)
+        finalGradeTV = findViewById(R.id.finalgrade_tv)
+
+        val closeButton = findViewById<Button>(R.id.close_button)
+        val restartButton = findViewById<Button>(R.id.restart_button)
+        closeButton.setOnClickListener {
+            closeApp()
+        }
+
+        restartButton.setOnClickListener {
+            startOver()
+        }
+
+        // Set UI Objects Text values
+        totalQuestionsTV!!.text = "There were $quizSize Questions."
+        totalCorrectTV!!.text = "Correct Answers: $numCorrect"
+        finalGradeTV!!.text = "$scorePer%"
+        if (scorePer >= 80) {
+            finalGradeTV!!.setTextColor(Color.GREEN)
+            commentTV!!.setTextColor(Color.GREEN)
+            commentTV!!.text = "OMEGA GOOD JOB!"
+        } else if (scorePer in 51..78) {
+            finalGradeTV!!.setTextColor(Color.YELLOW)
+            commentTV!!.setTextColor(Color.YELLOW)
+            commentTV!!.text = "YOU'RE ALMOST THERE!"
+        } else {
+            finalGradeTV!!.setTextColor(Color.RED)
+            commentTV!!.setTextColor(Color.RED)
+            commentTV!!.text = "BETTER STUDY!"
+        }
+    }
+
+    private fun startOver() {
+        // Returns to MainActivity page.
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun closeApp() {
+        // Closes the App
+        finishAffinity()
+    }
+}
