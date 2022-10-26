f58fe31105e5ab83a1b11c1a1d73bff3044c0d65
Nudennie <9339984+Tc2r1@users.noreply.github.com>
2022-10-26 10:22:25 -0500

    no message (#171)

--- Interview_Prep_App/native_kotlin/view_binding_/src/main/java/com/interviewprep/kotlinretrofit/repository/models/response/AnswersResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.interviewprep.kotlinretrofit.repository.models.response
-
-import com.google.gson.annotations.SerializedName
-import com.interviewprep.kotlinretrofit.repository.models.Answer
-
-data class AnswersResponse(
-    @SerializedName("answers")
-    var answers: ArrayList<Answer>
-)
