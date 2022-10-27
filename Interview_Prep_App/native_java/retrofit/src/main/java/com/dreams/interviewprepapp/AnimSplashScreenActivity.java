d4e1df3463b58c0b6d6f8f35ad942982e749142c
prasad-more-12 <prsadmore012@gmail.com>
2022-10-27 21:20:59 +0530

    First Commit

---
+++ Interview_Prep_App/native_java/retrofit/src/main/java/com/dreams/interviewprepapp/AnimSplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.dreams.interviewprepapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AnimSplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_anim_splash_screen);
+    }
+}
